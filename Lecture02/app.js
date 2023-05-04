7df113130fbc73e59df22c475979f8ad7f943716
Ivee Jintalan <iveejintalan17@gmail.com>
2023-05-04 09:35:56 +0800

    Added new folders

--- Lecture02/app.js
+++ Lecture02/app.js
@@ -17,12 +17,11 @@ function calculateNumericOutput() {
   student.name = document.getElementById('name').value;
 
   var totalNameValue = 0;
-
-  for(var i = 0; i < student.name.length; i++) {
+  for (var i = 0; i < student.name.length; i++) {
     totalNameValue += student.name.charCodeAt(i);
   }
 
   // Insert result into page
-  var output = "Total numeric value of person's name is " + totalNameValue;
+  var output = "Total Numeric value of person's name is " + totalNameValue;
   document.getElementById('output').innerText = output;
 }
