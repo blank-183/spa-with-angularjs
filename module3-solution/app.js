134fa623b88ff9dc6bec94c636ecaad875fc6ee6
Ivee Jintalan <iveejintalan17@gmail.com>
2023-05-23 17:07:52 +0800

    Update module 3 solution

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -2,15 +2,29 @@
   'use strict';
 
   angular.module('NarrowItDownApp', [])
-  .controller('NarrowItDownController', NarrowItDownController)
-  .service('MenuSearchService', MenuSearchService)
-  .directive('foundItems', FoundItems)
-  .constant('ApiBaseUrl', 'https://coursera-jhu-default-rtdb.firebaseio.com')
+    .controller('NarrowItDownController', NarrowItDownController)
+    .service('MenuSearchService', MenuSearchService)
+    .directive('foundItems', FoundItems)
+    .constant('ApiBaseUrl', 'https://coursera-jhu-default-rtdb.firebaseio.com')
 
   NarrowItDownController.$inject = ['MenuSearchService'];
   function NarrowItDownController() {
-    
+    var menu = this;
 
+  }
+
+  MenuSearchService.$inject = ['$http', 'ApiBaseUrl'];
+  function MenuSearchService($http, ApiBaseUrl) {
+    var service = this;
+  }
+
+  function FoundItems() {
+    var ddo = {
+      restrict: 'E',
+      templateUr: 'foundItems.html'
+    }
+
+    return ddo;
   }
 
 })();
